159ae3b8b21028efb67ddeaa1b5dfe0445c8ca96
Fencekeeper <vegard.fjellbo@gmail.com>
2020-01-21 11:23:29 +0100

    coded a preliminary GameDef trait

--- src/main/scala/paths/GameDef.scala
+++ src/main/scala/paths/GameDef.scala
@@ -1,5 +1,35 @@
 package paths
 
 trait GameDef {
+  case class Pos(row: Int, col: Int) {
+    def deltaRow = copy(row = row + 1)
+    def deltaCol = copy(col = col + 1)
+    def deltaRC = copy(row = row + 1, col = col + 1)
+  }
 
+  def startPos: Pos
+  def goal: Pos
+
+  type Terrain = Pos => Boolean
+
+  def terrain: Terrain
+
+  sealed abstract class Move
+  case object Right extends Move
+  case object Up extends Move
+  case object Diagonally extends Move
+
+  case class Block(pos: Pos) {
+    def right = Block(pos.deltaCol)
+    def up = Block(pos.deltaRow)
+    def diagonally = Block(pos.deltaRC)
+
+    def isLegal: Boolean = terrain(pos)
+
+    def neighbors: List[(Block, Move)] =
+      (right, Right) :: (up, Up) :: (diagonally, Diagonally) :: Nil
+
+    def legalNeighbors: List[(Block, Move)] =
+      neighbors.filter(bm => bm._1.isLegal)
+  }
 }
