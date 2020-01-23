9d22fed4ee78682627553297337b1aa594cabdb3
Fencekeeper <vegard.fjellbo@gmail.com>
2020-01-23 13:53:08 +0100

    added more operations to the case class Pos

--- src/main/scala/paths/GameDef.scala
+++ src/main/scala/paths/GameDef.scala
@@ -1,12 +1,34 @@
 package paths
 
+/**
+ * I have to totally rethink this. Maybe one could use a simple recursion to
+ * find all paths if necessary. However, this may be unnecessary. Maybe
+ * I can use the path that goes via (0, n) as startPos and the one
+ * that goes via (m, 0) as goal. Then I could explore, but the issue is
+ * that I would not like to exclude paths as a build pathchains. So maybe
+ * my problem is too different from this game?
+ *
+ * We could consider steps like replacing a Diagonally with a Right and an Up
+ * or vice versa.
+ *
+ * Can I build the paths and the path chains simultaneously?
+ *
+ * An idea. Maybe I could use this exercise to demonstrate property-based testing
+ * at the Itera conference and/or at some testforum? I would then have several tests
+ * that I can implement, like the counting of paths and the Euler characteristic.
+ */
+
 trait GameDef {
   case class Pos(row: Int, col: Int) {
-    def deltaRow = copy(row = row + 1)
-    def deltaCol = copy(col = col + 1)
-    def deltaRC = copy(row = row + 1, col = col + 1)
+    def deltaRow(d: Int) = copy(row = row + d)
+    def deltaCol(d: Int) = copy(col = col + d)
+    def deltaRC(d1: Int, d2: Int) = copy(row = row + d1, col = col + d2)
     def <=(that: Pos) = row <= that.row && col <= that.col
     def <(that: Pos) = this <= that && this != that
+
+    def left = deltaCol(-1)
+    def down = deltaRow(-1)
+    def diagonallyLeft = deltaRC(-1, -1)
   }
 
   type Terrain = Pos => Boolean
@@ -21,14 +43,14 @@ trait GameDef {
   case object Diagonally extends Move
 
   case class Block(pos: Pos) {
-    def right = Block(pos.deltaCol)
-    def up = Block(pos.deltaRow)
-    def diagonally = Block(pos.deltaRC)
+    def right = Block(pos.deltaCol(1))
+    def up = Block(pos.deltaRow(1))
+    def diagonallyRight = Block(pos.deltaRC(1, 1))
 
     def isLegal: Boolean = terrain(pos)
 
     def neighbors: List[(Block, Move)] =
-      (right, Right) :: (up, Up) :: (diagonally, Diagonally) :: Nil
+      (right, Right) :: (up, Up) :: (diagonallyRight, Diagonally) :: Nil
 
     def legalNeighbors: List[(Block, Move)] =
       neighbors.filter(bm => bm._1.isLegal)
